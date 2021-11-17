792c86682000e957c9c371f4724e5a645917c4e0
Aaron Bison <aaronbison97@gmail.com>
2021-11-18 00:03:00 +0200

    Minor changes Moved updateFile to a different function Code cleaning from hardcoded elements

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const cheerio = require('cheerio');
 const express = require('express');
 const fs = require('fs');
 
+// Control constants
 const url = 'https://www.emag.ro/telefon-mobil-apple-iphone-xr-64gb-red-mry62rm-a/pd/DKSY9VBBM/';
+
 const PORT = 8000;
+
 // 1800000 = half an hour interval
 const interval = 1800000;
+
+const observableElement = '.pricing-block'
 
 const app = express()
 let price = 0;
@@ -16,21 +21,23 @@ function scraping() {
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
-            $('.pricing-block', html).each(function () {
+            $(observableElement, html).each(function () {
                 price = $(this).text()
             })
-
-            fs.open('IPhoneXR_log.txt', 'a', 666, function (e, id) {
-
-                fs.write(id, new Date() + price + "\r\n", null, 'utf8', function () {
-                    fs.close(id, function () {
-                        console.log('File was updated!');
-                    });
-                })
-            });
+            updateFile()
         }).catch(err => console.log(err))
 }
 
+function updateFile() {
+    fs.open('IPhoneXR_log.txt', 'a', 666, function (e, id) {
+
+        fs.write(id, new Date() + price + "\r\n", null, 'utf8', function () {
+            fs.close(id, function () {
+                console.log('File was updated with: ' + price);
+            });
+        })
+    });
+}
 
 setInterval(() => {
     scraping()
